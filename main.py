75c5bc04abacacc11f8e3951c630fffc3218e025
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-12 20:44:12 +0100

    feat: Blink onboard led while connecting

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ led = Pin(2, Pin.OUT)
 def connect_wifi(ssid, password):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
+        blink_led()
         sta_if.active(True)
         sta_if.connect(ssid, password)
         while not sta_if.isconnected():
+            blink_led(2)
             time.sleep(1)
     print('Network config:', sta_if.ifconfig())
+    blink_led(2, 3) # Blink 2 times with 3 seconds interval
 
 
 def toggle_led():
     led.value(not led.value())
     
 
-def blink_led(count=2):
+def blink_led(count=1, blink_len=1):
+    count *= 2 # Toggle on, toggle off
     for i in range(blink_led):
         toggle_led()
-        time.sleep(1)
+        time.sleep(blink_len)
 
 connect_wifi(os.environ['SSID'], os.environ['PASSWORD'])
