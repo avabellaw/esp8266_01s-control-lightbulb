6977756faf58b8071a8c48ec41d4f69269b3da64
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-12 20:39:58 +0100

    feat: Add connect_wifi and toggle_led functions

---
+++ main.py
@@ -0,0 +1,20 @@
+import network
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+def connect_wifi(ssid, password):
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            time.sleep(1)
+    print('Network config:', sta_if.ifconfig())
+
+
+def toggle_led():
+    led.value(not led.value())
+
+connect_wifi(os.environ['SSID'], os.environ['PASSWORD'])
