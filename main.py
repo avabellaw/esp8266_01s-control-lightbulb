05765a4ad9287367d7f94b0aba29d68a105100da
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-12 21:16:01 +0100

    feat: Add listen for button click function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from machine import Pin
 
 led = Pin(2, Pin.OUT)
+button = Pin(2, Pin.IN, Pin.PULL_UP)
 
 def connect_wifi(ssid, password):
     sta_if = network.WLAN(network.STA_IF)
@@ -25,5 +26,15 @@ def blink_led(count=1, blink_len=0.1):
     for i in range(count):
         toggle_led()
         time.sleep(blink_len)
+        
+def listen_for_btn_click():
+    while True:
+        if button.value() == 0:
+            blink_led(1, 0.3)
+            while button.value() == 0:
+                pass  # Wait for the button to be released
+        time.sleep(0.1)  # So it doesn't accidentally trigger multiple times
 
 connect_wifi(os.environ['SSID'], os.environ['PASSWORD'])
+
+
