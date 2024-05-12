9f3c4c4f7b2751d08e55796ec04c73109f2d1b08
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-12 21:02:45 +0100

    Increase speed of blinks when connecting to WIFI

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ def connect_wifi(ssid, password):
         sta_if.active(True)
         sta_if.connect(ssid, password)
         while not sta_if.isconnected():
-            blink_led(2)
-            time.sleep(1)
+            blink_led(1, 1)
     print('Network config:', sta_if.ifconfig())
-    blink_led(2, 3) # Blink 2 times with 3 seconds interval
+    blink_led(5, 0.1) # Blink 5 times with 0.1 seconds interval
 
 
 def toggle_led():
     led.value(not led.value())
     
 
-def blink_led(count=1, blink_len=1):
+def blink_led(count=1, blink_len=0.1):
     count *= 2 # Toggle on, toggle off
     for i in range(count):
         toggle_led()
