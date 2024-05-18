5d3b74000cefda37559db911b641257707a6a2e7
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-05-18 18:12:03 +0100

    Use variable for LED_OFF  + add sleep in loops
    I also removed redundant code

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import config
 led = Pin(2, Pin.OUT)  # GPIO2
 button = Pin(0, Pin.IN, Pin.PULL_UP)  # GPIO0
 
+# 1 for interal led, 0 for external led
+LED_OFF = 0
+
+led.value(LED_OFF)  # Ensure led is off at start
+
 
 def toggle_led():
     led.value(not led.value())
@@ -15,13 +20,13 @@ def toggle_led():
 
 def blink_led(count=1, blink_len=0.1):
     count *= 2  # Toggle on, toggle off
+    # Common anode configuaration and therefore, 1 is off
+    # (cathode to ground)
+    led.value(LED_OFF)  # Ensure led is off before blinking
     for i in range(count):
         toggle_led()
         time.sleep(blink_len)
-
-    # Common anode configuaration and therefore, 1 is off
-    # (cathode to ground)
-    led.value(1)  # Ensure led is off after blinking
+    led.value(LED_OFF)  # Ensure led is off after blinking
 
 
 def connect_wifi(ssid, password):
@@ -32,23 +37,24 @@ def connect_wifi(ssid, password):
         sta_if.connect(ssid, password)
         while not sta_if.isconnected():
             blink_led(1, 1)
-    print(f'Connected to WiFi: {ssid}\n\
-            Network config: {sta_if.ifconfig()}')
-    blink_led(4, 0.2)  # Blink 5 times with 0.2 second interval
+        print(f'Connected to WiFi: {ssid}\n\
+                Network config: {sta_if.ifconfig()}')
+        blink_led(4, 0.2)  # Blink 5 times with 0.2 second interval
 
 
 def listen_for_btn_click(socket):
     while True:
         try:
             data = socket.recv(1)
+            time.sleep(0.3)
         except OSError:
-            pass
+            time.sleep(0.1)
         else:
             socket.send(data)
 
         if button.value() == 0:  # 0 when button is pressed
             send_message(socket, config.BUTTON_CLICK)
-            blink_led(2, 0.1)
+            blink_led(1, 0.1)
             while button.value() == 0:
                 pass  # Wait for the button to be released
 
@@ -56,6 +62,8 @@ def listen_for_btn_click(socket):
 def connect_to_server():
     socket = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
     server_address = (config.SERVER_IP, config.SERVER_PORT)
+    
+    # Need to add feedback if not connected
     socket.connect(server_address)
 
     socket.setblocking(False)
@@ -66,18 +74,6 @@ def connect_to_server():
 def send_message(socket, message):
     print('Sending message:', message)
     socket.send(message)
-
-
-def listen_for_ping(socket):
-    socket.settimeout(2)
-    while True:
-        try:
-            data = socket.recv(1)
-            socket.send(data)
-            if not data:
-                break
-        except usocket.timeout:
-            pass
 
 
 connect_wifi(config.SSID, config.PASSWORD)
