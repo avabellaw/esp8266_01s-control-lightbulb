70438d5946e7d3496dc422b5c8ed028c914ce2e6
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-07 23:16:22 +0100

    Add brightness control to control_lightbulb

--- control_lightbulb.py
+++ control_lightbulb.py
@@ -1,6 +1,18 @@
 import os
-import env # noqa
+import env  # noqa
 import tinytuya
+from enum import Enum
+
+
+class BrightnessLevel(Enum):
+    LOW = (10, 330)
+    MEDIUM = (340, 660)
+    HIGH = (670, 1000)
+
+
+LOW_BRIGHTNESS_LEVEL = BrightnessLevel.LOW.value[0]
+MED_BRIGHTNESS_LEVEL = (BrightnessLevel.MEDIUM.value[0] + BrightnessLevel.MEDIUM.value[1]) / 2
+HIGH_BRIGHTNESS_LEVEL = BrightnessLevel.HIGH.value[1]
 
 
 class LightBulb:
@@ -20,11 +32,61 @@ class LightBulb:
 
         data = self.light.status()
         print(data)
-        self.is_on = data['dps']['20']
+        dps = data['dps']
+        self.is_on = dps['20']
+        self.brightness = dps['22']
+        self.brightness_level = self.get_brightness_level()
+        print(self.brightness)
+
+    def get_status(self):
+        dps = self.light.status()['dps']
+        return {"is_on": dps['20'],
+                "brightness": dps['22']}
+
+    def get_brightness_level(self):
+        print(f"brighness: {self.brightness}")
+        print(f"Brightness high 1: {BrightnessLevel.HIGH.value[0]}\
+            Brightness high 2: {BrightnessLevel.HIGH.value[1]}\
+            in range: {self.brightness in range(BrightnessLevel.HIGH.value[0], BrightnessLevel.HIGH.value[1] + 1)}\
+            ")
+        if self.brightness in range(BrightnessLevel.LOW.value[0],
+                                    BrightnessLevel.LOW.value[1]):
+            return BrightnessLevel.LOW
+        elif self.brightness in range(BrightnessLevel.MEDIUM.value[0],
+                                      BrightnessLevel.MEDIUM.value[1]):
+            return BrightnessLevel.MEDIUM
+        elif self.brightness in range(BrightnessLevel.HIGH.value[0],
+                                      BrightnessLevel.HIGH.value[1] + 1):
+            return BrightnessLevel.HIGH
 
     def toggle(self):
         self.light.turn_off() if self.is_on else self.light.turn_on()
         self.is_on = not self.is_on
 
+    def get_next_brightness_level(self):
+        current_level = self.get_brightness_level()
 
-light = LightBulb()
+        if current_level == BrightnessLevel.LOW:
+            return MED_BRIGHTNESS_LEVEL
+        elif current_level == BrightnessLevel.MEDIUM:
+            return HIGH_BRIGHTNESS_LEVEL
+        elif current_level == BrightnessLevel.HIGH:
+            return LOW_BRIGHTNESS_LEVEL
+
+    def toggle_brightness(self):
+        status = self.get_status()
+        self.brightness = status["brightness"]
+        print(f"Current brightness: {self.brightness}")
+        self.brightness = int(self.get_next_brightness_level())
+        print(f"Setting brightness to {self.brightness}")
+
+        self.light.set_brightness(self.brightness)
+
+
+def get_lightbulb_instance():
+    try:
+        light = LightBulb()
+        return light
+    except Exception as e:
+        print(f"Issue with lightbulb: {e}")
+        return None
